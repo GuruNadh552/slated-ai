b714a0c291b39b103fc8f383a9dc0032bf73b210
Your Name <you@example.com>
2022-02-10 23:31:08 +0530

    Changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'slated-ai';
+  icons : string[] = ['../assets/icons/email.svg','../assets/icons/call.svg','../assets/icons/briefcase.svg']
+
+  socialIcons:any=[
+  {
+    "iconUrl":"../assets/icons/linkedin.svg",
+    "text":"LinkedIn"
+  },
+  {
+    "iconUrl":"../assets/icons/github.svg",
+    "text":"Github"
+  },
+  {
+    "iconUrl":"../assets/icons/briefcase.svg",
+    "text":"Facebook"
+  },
+  {
+    "iconUrl":"../assets/icons/twitter.svg",
+    "text":"Twitter"
+  }]
+
+  data:any={
+    email:"guyawah@gmail.com",
+    title:"Co-founder Apple",
+    phone:"+2 383 282 2822"
+  }
+
+  valueChanged(ind:Number,event:any){
+    if(ind==0)
+      this.data.email = event.target.value;
+    else if(ind==1)
+      return this.data.title = event.target.value;
+    else this.data.phone = event.target.value;
+    console.log(this.data);
+
+  }
+
+  returnDatafromIndex(ind:Number){
+    if(ind==0)
+      return this.data.email
+    if(ind==1)
+      return this.data.title;
+    return this.data.phone;
+  }
 }
